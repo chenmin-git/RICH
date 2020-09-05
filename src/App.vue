b1c53fcc2a6e6c6a1883a8d2617ccb708c513962
chenmin <2857135639@qq.com>
2020-09-05 11:56:43 +0800

    '第一次提交'

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
-
+<script>
+	export default{
+		mounted() {
+		      if (this._isMobile()) {
+		        alert("手机端");
+		        // this.$router.replace('/m_index');
+		      } else {
+		        alert("pc端");
+		        // this.$router.replace('/pc_index');
+		      }
+		    },
+		    methods: {
+		      //App.vue
+		      _isMobile() {
+		        let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+		        return flag;
+		      }
+		  }
+	}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+	我是头部
+</template>
+
+<script>
+	export default{
+		
+	}
+</script>
+
+<style>
+</style>
